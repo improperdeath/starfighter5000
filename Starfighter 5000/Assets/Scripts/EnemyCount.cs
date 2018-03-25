5864f89587a3063c9feddb4f970c1464280acf85
Matt Dean <mattdean98@gmail.com>
2018-03-25 09:45:05 -0600

    set up UI, hid powerup icon, started designing enemy count in enemyCount.cs

---
+++ Starfighter 5000/Assets/Scripts/EnemyCount.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyCount : MonoBehaviour {
+
+    public Text enemyCountText;
+    public int enemyCount;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        enemyCountText.text = "Enemies Remaining: " + enemyCount;
+	}
+}
